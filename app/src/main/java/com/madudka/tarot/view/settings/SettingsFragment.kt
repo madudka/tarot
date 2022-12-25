250b016aa33b3f7186b01e8c1e380feb09f311b9
m.a.dudka <madudka.dev@gmail.com>
2022-12-25 16:39:48 +0300

    Fixed some bugs. Add show case to settings fragment.

--- app/src/main/java/com/madudka/tarot/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/madudka/tarot/view/settings/SettingsFragment.kt
@@ -7,17 +7,22 @@ import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.core.view.get
+import androidx.core.view.size
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.madudka.tarot.R
 import com.madudka.tarot.databinding.SettingsFragmentBinding
 import com.madudka.tarot.glide.GlideApp
 import com.madudka.tarot.glide.loadCardBackImage
 import com.madudka.tarot.services.SoundService
+import com.madudka.tarot.utils.showCase
 import com.madudka.tarot.utils.showCleanCacheDialog
 import com.madudka.tarot.utils.showInternetConnectionDialog
 import com.madudka.tarot.view.App
@@ -27,6 +32,8 @@ import com.madudka.tarot.view.adapter.OnItemClickListener
 import com.madudka.tarot.view.adapter.SettingsStylesAdapter
 import com.madudka.tarot.viewmodel.settings.SettingsViewModel
 import kotlinx.coroutines.*
+
+private const val SETTINGS_KEY = "SETTINGS_KEY"
 
 class SettingsFragment : BaseFragment<List<String>>() {
 
@@ -69,15 +76,7 @@ class SettingsFragment : BaseFragment<List<String>>() {
             }
         }
 
-        val manager = GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
-        val animation = AnimationUtils.loadLayoutAnimation(requireContext(), R.anim.layout_anim_fall_down)
-        settingsStylesAdapter.clickListener = clickListener
-        binding.rvStyles.apply {
-            adapter = settingsStylesAdapter
-            layoutManager = manager
-            layoutAnimation = animation
-            setHasFixedSize(true)
-        }
+        setupRecyclerView()
 
         viewModel.getPrefixes().observe(viewLifecycleOwner){
             setData(it)
@@ -109,6 +108,38 @@ class SettingsFragment : BaseFragment<List<String>>() {
         listData?.let {
             settingsStylesAdapter.updateData(it)
             binding.rvStyles.scheduleLayoutAnimation()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val manager = GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
+        val animation = AnimationUtils.loadLayoutAnimation(requireContext(), R.anim.layout_anim_fall_down)
+        settingsStylesAdapter.clickListener = clickListener
+        binding.rvStyles.apply {
+            adapter = settingsStylesAdapter
+            layoutManager = manager
+            layoutAnimation = animation
+            setHasFixedSize(true)
+            addOnItemTouchListener(object : RecyclerView.OnItemTouchListener{
+                override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                    return true
+                }
+
+                override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+                    if (binding.rvStyles.size > 0){
+                        showCase(
+                            requireActivity(),
+                            binding.rvStyles[0],
+                            getString(R.string.taro_style_hint),
+                            SETTINGS_KEY
+                        )
+                    }
+                }
+
+                override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+
+                }
+            })
         }
     }
 
